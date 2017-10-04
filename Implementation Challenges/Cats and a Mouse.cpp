7591a1ed01a93499540b3dcf7cc558911e002d45
Harishiv Singh <harishiv8@gmail.com>
2017-10-04 23:56:09 +0530

    Create Cats and a Mouse.cpp

---
+++ Implementation Challenges/Cats and a Mouse.cpp
@@ -0,0 +1,44 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int q;
+    cin >> q;
+    for(int a0 = 0; a0 < q; a0++)
+    {
+        int x;
+        int y;
+        int z;
+        cin >> x >> y >> z;
+        if( (sqrt((z - x) * (z - x))) < (sqrt((z - y) * (z - y))) )
+            cout<<"Cat A\n";
+        else if( (sqrt((z - x) * (z - x))) > (sqrt((z - y) * (z - y))) )
+            cout<<"Cat B\n";
+        else
+            cout<<"Mouse C\n";
+    }
+    return 0;
+}
